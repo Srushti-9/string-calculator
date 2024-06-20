5119320745829249d3fccb57c1f72b9d12906195
Srushti-9 <srushtisuvarna77@gmail.com>
2024-06-21 01:43:31 +0530

    Add test for supporting different delimiters

--- __tests__/StringCalculator.test.ts
+++ __tests__/StringCalculator.test.ts
@@ -28,3 +28,8 @@ test('should handle new lines between numbers', () => {
   const calculator = new StringCalculator();
   expect(calculator.add('1\n2,3')).toBe(6);
 });
+
+test('should support different delimiters', () => {
+  const calculator = new StringCalculator();
+  expect(calculator.add('//;\n1;2')).toBe(3);
+});
