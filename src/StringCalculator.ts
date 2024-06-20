808328f7331e22a241e7d1736e13d83b203411e2
Srushti-9 <srushtisuvarna77@gmail.com>
2024-06-21 00:52:00 +0530

    Add the logic for an empty string input returning 0

---
+++ src/StringCalculator.ts
@@ -0,0 +1,11 @@
+// src/StringCalculator.ts
+
+export class StringCalculator {
+    add(numbers: string): number {
+      if (numbers === '') {
+        return 0;
+      }
+      return 0; // This will be updated in future steps
+    }
+  }
+  
